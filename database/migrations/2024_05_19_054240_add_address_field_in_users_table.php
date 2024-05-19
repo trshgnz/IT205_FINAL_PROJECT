4c94f89ed8014c52987e02d1de37b6798f7ea235
Kieffer <kganza@traxiontech.net>
2024-05-19 13:44:53 +0800

    address migraiton

--- database/migrations/2024_05_19_054240_add_address_field_in_users_table.php
+++ database/migrations/2024_05_19_054240_add_address_field_in_users_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('address')->default('default address');
+            $table->string('phone')->default('default phone');
         });
     }
 
@@ -22,7 +23,8 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn(['address', 'phone']);
+
         });
     }
 };
